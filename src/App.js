1073f20a6c8d5dbf77f48d22b58eec265a7273b8
BrahyamLunaVL <brahyam.luna@virtuallatinos.com>
2024-05-21 17:17:36 -0500

    Inputs created

--- src/App.js
+++ src/App.js
@@ -3,21 +3,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <InputComponent
+      inputClass=""
+      inputId="name"
+      inputLabel="Name"
+      type="text"
+      inputPlaceHolder="Enter your name"
+      message=""
+    />
+      <InputComponent
+      inputClass="error"
+      inputId="password"
+      inputLabel="Password"
+      type="password"
+      inputPlaceHolder="Enter your password"
+      message="The password you entered is incorrect"
+    />
+    <InputComponent
+      inputClass="good"
+      inputId="email"
+      inputLabel="Email"
+      type="email"
+      inputPlaceHolder="Enter your email"
+      message="Email is available"
+    />
+    </main>
+  );
+}
+
+function InputComponent({inputClass, inputId, inputLabel, type, inputPlaceHolder, message}){
+  return(
+    <div id='input-container' className={inputClass}> 
+      <label className='form-label' for={inputId}>{inputLabel}</label>
+      <input className='form-input' type={type} placeholder={inputPlaceHolder} id={inputId}/>
+      <div>
+        <i class='fa-solid fa-triangle-exclamation alert'></i>
+        <i class="fa-solid fa-check check"></i>
+        <p>{message}</p>
+      </div>
     </div>
   );
 }
